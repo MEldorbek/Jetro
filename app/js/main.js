bd71a4f13ce30c49659212a8f4c377b10cd47f69
SkeLeT-LIGHT <eldorbekm94@gmail.com>
2020-08-10 16:45:01 +0500

    added slider

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow_left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow_right.svg" alt=""></button>',
+        asNavFor: '.thumbs',
+
+    });
+    $('.thumbs').slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        asNavFor: '.slider',
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true,
+    });
+});
