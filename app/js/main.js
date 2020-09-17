f504570b1168da8b987b53276751b93d6f611692
SkeLeT-LIGHT <eldorbekm94@gmail.com>
2020-09-17 22:13:32 +0500

    added mobile_style

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
+        focusOnSelect: true,
         arrows: true,
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow_left.svg" alt=""></button>',
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow_right.svg" alt=""></button>',
@@ -10,11 +11,16 @@ $(document).ready(function() {
     });
     $('.thumbs').slick({
         slidesToShow: 6,
-        slidesToScroll: 6,
+        slidesToScroll: 1,
         asNavFor: '.slider',
         dots: false,
         arrows: false,
         centerMode: true,
         focusOnSelect: true,
+
+    });
+
+    $('.header__menu-btn').on('click', function() {
+        $('.header__menu > ul').slideToggle();
     });
 });
